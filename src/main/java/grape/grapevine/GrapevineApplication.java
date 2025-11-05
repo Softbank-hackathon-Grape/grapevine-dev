a46910f2110158f1a69c997aac2abd3b62153323
Ji-su-Shin <caujisu2020@gmail.com>
2025-11-05 15:21:17 +0900

    init

---
+++ src/main/java/grape/grapevine/GrapevineApplication.java
@@ -0,0 +1,13 @@
+package grape.grapevine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapevineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrapevineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grape/grapevine/GrapevineApplicationTests.java
@@ -0,0 +1,13 @@
+package grape.grapevine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapevineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
