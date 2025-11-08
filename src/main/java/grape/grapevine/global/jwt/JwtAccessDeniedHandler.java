81fd99aa7d09bacfcb40cbf48c6aff1176ff86dd
noeyah2 <hy0708@cau.ac.kr>
2025-11-08 22:10:57 +0900

    chore(jwt): JwtAccessDeniedHandler
    비즈니스로직을 위한 단순 error -> status, contenttype, characterencoding으로 수정

--- src/main/java/grape/grapevine/global/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/grape/grapevine/global/jwt/JwtAccessDeniedHandler.java
@@ -1,14 +1,22 @@
 package grape.grapevine.global.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import grape.grapevine.global.BaseResponse;
+import grape.grapevine.global.ErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+  private final ObjectMapper objectMapper;
 
   @Override
   public void handle(
@@ -16,7 +24,9 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
       HttpServletResponse response,
       AccessDeniedException accessDeniedException)
       throws IOException {
-    // 필요한 권한이 없이 접근하려 할때 403
-    response.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden");
+    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    response.setCharacterEncoding("UTF-8");
+    objectMapper.writeValue(response.getWriter(), BaseResponse.fail(ErrorCode.HANDLE_ACCESS_DENIED));
   }
 }
