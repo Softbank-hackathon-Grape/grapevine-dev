eb515b836eee14195de86c64036bf2ee3b68cc93
noeyah2 <hy0708@cau.ac.kr>
2025-11-08 21:11:02 +0900

    feat(jwt): JwtAccessDeniedHandler, JwtAuthenticationEntryPoint, JwtAuthenticationFilter, JwtTokenProvider
    config filter에 필요한 코드 추가

---
+++ src/main/java/grape/grapevine/global/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package grape.grapevine.global.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  public static final String AUTHORIZATION_HEADER = "Authorization";
+  public static final String BEARER_PREFIX = "Bearer ";
+
+  private final JwtTokenProvider jwtTokenProvider;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws
+      IOException,
+      ServletException {
+
+    String jwt = resolveToken(request);
+
+    filterChain.doFilter(request, response);
+  }
+
+  private String resolveToken(HttpServletRequest request) {
+    String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+    if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+      return bearerToken.substring(7);
+    }
+    return null;
+  }
+}
