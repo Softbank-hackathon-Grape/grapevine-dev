a0bc4f6256f7b13da8984f18331edbdf4e97c128
Ji-su-Shin <caujisu2020@gmail.com>
2025-11-08 14:43:26 +0900

    feat(user): Controller, Service, Repository 생성

---
+++ src/main/java/grape/grapevine/application/user/UserService.java
@@ -0,0 +1,10 @@
+package grape.grapevine.application.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+}
